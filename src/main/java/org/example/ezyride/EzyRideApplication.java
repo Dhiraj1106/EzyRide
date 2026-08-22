a07edb457814ef8c6567fcf88122f7187134a072
Dhiraj1106 <dhirajsarangi420@gmail.com>
2026-08-22 10:24:52 +0530

    Initial commit

---
+++ src/main/java/org/example/ezyride/EzyRideApplication.java
@@ -0,0 +1,13 @@
+package org.example.ezyride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzyRideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzyRideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ezyride/EzyRideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ezyride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzyRideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
